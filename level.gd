8ee8643e2bea2dc1d0498cb72aa82d65dd91b119
blah42 <stevencoulter1808@yahoo.com>
2026-07-17 14:36:30 -0400

    scrolling map

--- level.gd
+++ level.gd
@@ -1,11 +1,27 @@
 extends Node2D
 
-
+var map_speed = 200 
+const screen_height = 9*16
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var character = preload("res://Skiier.tscn").instantiate()
+	$Player.add_child(character, true)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	mapMove(map_speed,delta)
 	pass
+func mapMove(speed,delta):
+	
+	$Map.move_local_y(-delta*speed)
+	if($Map/MapSegment0.global_position.y < -150*2):
+		$Map/MapSegment0.move_local_y(screen_height*5)
+	if($Map/MapSegment1.global_position.y < -150*2): 
+		$Map/MapSegment1.move_local_y(screen_height*5)
+	if($Map/MapSegment2.global_position.y < -150*2):
+		$Map/MapSegment2.move_local_y(screen_height*5)
+	if($Map/MapSegment3.global_position.y < -150*2): 
+		$Map/MapSegment3.move_local_y(screen_height*5)
+	if($Map/MapSegment4.global_position.y < -150*2): 
+		$Map/MapSegment4.move_local_y(screen_height*5)
