f05cb4016b0bad499e44384593a605617ba10a0c
mmann6152 <88596909+mmann6152@users.noreply.github.com>
2026-07-17 19:07:45 -0400

    collision detection implemented

--- skiier.gd
+++ skiier.gd
@@ -24,3 +24,8 @@ func _physics_process(delta: float) -> void:
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
+		
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	print("Detecion")
