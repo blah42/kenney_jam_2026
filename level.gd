3ecdc3869c44908b34698264bd0a706ec0fe6fc7
blah42 <stevencoulter1808@yahoo.com>
2026-07-17 18:54:44 +0000

    fixed level.gd

--- level.gd
+++ level.gd
@@ -1,23 +1,15 @@
 extends Node2D
 
-<<<<<<< HEAD
 var map_speed = 200 
 const screen_height = 9*16
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var character = preload("res://Skiier.tscn").instantiate()
 	$Player.add_child(character, true)
-=======
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
->>>>>>> 2e76541 (updated character)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-<<<<<<< HEAD
 	mapMove(map_speed,delta)
 	pass
 func mapMove(speed,delta):
@@ -33,6 +25,3 @@ func mapMove(speed,delta):
 		$Map/MapSegment3.move_local_y(screen_height*5)
 	if($Map/MapSegment4.global_position.y < -150*2): 
 		$Map/MapSegment4.move_local_y(screen_height*5)
-=======
-	pass
->>>>>>> 2e76541 (updated character)
