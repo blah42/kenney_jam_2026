58dd5c1bada809b2117dd77d13fc8dfd64bea458
mmann6152 <88596909+mmann6152@users.noreply.github.com>
2026-07-17 13:36:53 -0400

    updated skiier

--- skiier.gd
+++ skiier.gd
@@ -10,3 +10,8 @@ func _physics_process(delta: float) -> void:
 	var inputDir = Input.get_vector("Move Left", "Move Right", "Move Up", "Move Down")
 	velocity = inputDir * SPEED
 	move_and_slide()
+	
+	if velocity.length() > 0:
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
