552c302e18b68d75d34a1e8862b3d66e1b91b312
blah42 <stevencoulter1808@yahoo.com>
2026-07-17 18:46:34 -0400

    added random map selection

--- level.gd
+++ level.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 var map_speed = 200 
 const screen_height = 9*16
+var map1 = preload("res://Assets/MapSegments/MapSegment1.res")
+var map2 = preload("res://Assets/MapSegments/MapSegment2.res")
+var map3 = preload("res://Assets/MapSegments/MapSegment3.res")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var character = preload("res://Skiier.tscn").instantiate()
@@ -14,13 +17,25 @@ func _process(delta: float) -> void:
 	pass
 func mapMove(speed,delta):
 	$Map.move_local_y(-delta*speed)
-	if($Map/MapSegment0.global_position.y < -150*2):
-		$Map/MapSegment0.move_local_y(screen_height*5)
-	if($Map/MapSegment1.global_position.y < -150*2): 
-		$Map/MapSegment1.move_local_y(screen_height*5)
-	if($Map/MapSegment2.global_position.y < -150*2):
-		$Map/MapSegment2.move_local_y(screen_height*5)
-	if($Map/MapSegment3.global_position.y < -150*2): 
-		$Map/MapSegment3.move_local_y(screen_height*5)
-	if($Map/MapSegment4.global_position.y < -150*2): 
-		$Map/MapSegment4.move_local_y(screen_height*5)
+	for i in $Map.get_children(false):
+		#print(i)
+		if(i.global_position.y < -screen_height*2):
+			i.move_local_y(screen_height*5)
+			var c = i.global_position
+			print(i)
+			i.queue_free()
+			var randMap = randi_range(1,3)
+			var d
+			match randMap:
+				1:
+					d = map1.instantiate()
+				2:
+					d = map2.instantiate()
+				3:
+					d = map3.instantiate()
+				_:
+					d = map3.instantiate()
+			d.global_position = c
+			$Map.add_child(d)
+			d.global_position = c
+pass
