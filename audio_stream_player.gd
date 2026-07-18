5e5a6472b9991d28714ef8f659faff31733d2c8b
mmann6152 <88596909+mmann6152@users.noreply.github.com>
2026-07-18 17:15:33 -0400

    new music

--- audio_stream_player.gd
+++ audio_stream_player.gd
@@ -1,15 +1,12 @@
-extends Node
-var score = 0
-var playerVelocity = Vector2(0,0)
-var playerState = 0
-var tutorial = 0
-var music : AudioStreamPlayer
+extends AudioStreamPlayer
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#music.play()
-	pass # Replace with function body.
+	$"/root/AudioStreamPlayerGlobal".play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	#$"/root/AudioStreamPlayerGlobal".play()
 	pass
