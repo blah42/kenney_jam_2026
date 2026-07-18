2800d06684f49432e01669bdbbc230d189eeb1ef
mmann6152 <88596909+mmann6152@users.noreply.github.com>
2026-07-18 13:27:47 -0400

    level difficulty changes

--- level.gd
+++ level.gd
@@ -12,9 +12,16 @@ var map7 = preload("res://Assets/MapSegments/MapSegment8.res")
 var map8 = preload("res://Assets/MapSegments/MapSegment9.res")
 var map9 = preload("res://Assets/MapSegments/MapSegment10.res")
 var map10 = preload("res://Assets/MapSegments/MapSegment11.res")
-var scoretime = 0
+var map11 = preload("res://Assets/MapSegments/MapSegment12.res")
+var map12 = preload("res://Assets/MapSegments/MapSegment13.res")
+var map13 = preload("res://Assets/MapSegments/MapSegment14.res")
+var map14 = preload("res://Assets/MapSegments/MapSegment15.res")
+var map15 = preload("res://Assets/MapSegments/MapSegment16.res")
 
+var scoretime = 0
+var lowerBound = 5
 var wolfProb = 1
+var wolfRate = 1000
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var character = preload("res://Skiier.tscn").instantiate()
@@ -25,7 +32,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	wolfProb = randi()%100
+	if Global.score>1000:
+		lowerBound = 16
+		wolfRate = 200
+	wolfProb = randi()%wolfRate
 	if $Enemies.get_child_count()==0:
 		if wolfProb==1:
 			var wolfy = preload("res://wolf.tscn").instantiate()
@@ -44,25 +54,22 @@ func _process(delta: float) -> void:
 func mapMove(speed,delta):
 	$Map.move_local_y(-delta*speed)
 	for i in $Map.get_children(false):
-		#print(i)
 		if(i.global_position.y < -screen_height*2):
 			i.move_local_y(screen_height*5)
 			var c = i.global_position
-			#print(i)
 			i.queue_free()
-			#var randMap = randi_range(1,11)
-			var randMap = 9
+			var randMap = randi_range(1,lowerBound)
 			var d
 			match randMap:
-				1:
+				11:
 					d = map1.instantiate()
-				2:
+				12:
 					d = map2.instantiate()
-				3:
+				13:
 					d = map3.instantiate()
-				4:
+				14:
 					d = map4.instantiate()
-				5:
+				15:
 					d = map5.instantiate()
 				6:
 					d = map6.instantiate()
@@ -74,6 +81,16 @@ func mapMove(speed,delta):
 					d = map9.instantiate()
 				10:
 					d = map10.instantiate()
+				1:
+					d = map11.instantiate()
+				2:
+					d = map12.instantiate()
+				3:
+					d = map13.instantiate()
+				4:
+					d = map14.instantiate()
+				5:
+					d = map15.instantiate()
 				_:
 					d = map3.instantiate()
 			d.global_position = c
