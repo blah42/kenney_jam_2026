42675d140970360ce9426405c8d8a959d821229a
mmann6152 <88596909+mmann6152@users.noreply.github.com>
2026-07-18 21:50:36 -0400

    small fixes

--- global.gd
+++ global.gd
@@ -4,6 +4,7 @@ var playerVelocity = Vector2(0,0)
 var playerState = 0
 var tutorial = 0
 var music : AudioStreamPlayer
+var highScore = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#music.play()
